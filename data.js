ea34c96d96f005c2780de5d0ff3ea0f89c575c03
TheSweetSpot-TSS <tssthesweetspot@gmail.com>
2026-09-23 12:43:03 +0530

    Change cheesecake images from SVG to JPEG format
    Updated image paths for cheesecake products to use JPEG format.

--- data.js
+++ data.js
@@ -35,22 +35,22 @@ const SITE = {
       products: [
         {
           name: "Biscoff Cheesecake - ₹215",
-          image: "images/Biscoff1.svg",
+          image: "images/Biscoff1.jpeg",
           description: "Creamy cheesecake with a buttery Biscoff base."
         },
         {
           name: "Nutella Cheesecake - ₹215",
-          image: "images/Nutella1.svg",
+          image: "images/Nutella1.jpeg",
           description: "Rich chocolate-hazelnut cheesecake for serious cravings."
         },
         {
           name: "Blueberry Cheesecake - ₹215",
-          image: "images/Blueberry1.svg",
+          image: "images/Blueberry1.jpeg",
           description: "Velvety cheesecake finished with blueberry compote."
         },
 		{
           name: "Chocolate Cheesecake - ₹215",
-          image: "images/Chocolate1.svg",
+          image: "images/Chocolate1.jpeg",
           description: "Velvety cheesecake finished with blueberry compote."
         }
       ]
